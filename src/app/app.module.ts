b96f601e0f2ae1645ae77fa1ddc22400e3dbe7f0
gaston becerra <gaston.becerra@gmail.com>
2023-06-26 01:56:27 -0300

    con items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { JuecesComponent } from './jueces/jueces.component';
@@ -13,7 +13,8 @@ import { JuecesComponent } from './jueces/jueces.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/jueces/jueces.component.ts
+++ src/app/jueces/jueces.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-jueces',
@@ -13,15 +15,60 @@ export class JuecesComponent {
   dimensions: any[] = [];
   items: any[] = [];
   fit: any[] = [];
+
+  surveyForm!: FormGroup; 
+  results: any[] = [];
   
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient, 
+    private formBuilder: FormBuilder
+  ) {}
+
+  getItemsFormArray(): FormArray | null {
+    return this.surveyForm.get('items') as FormArray | null;
+  }
+
+  submitSurveyForm() {
+    const itemsFormArray = this.getItemsFormArray();
+  
+    if (itemsFormArray) {
+      const formData = {
+        items: itemsFormArray.value
+      };
+  
+      console.log(formData);
+    }
+  }
 
   ngOnInit() {
     this.http.get<any>('assets/survey.json').subscribe(data => {
+  
       this.information = data['information'];
       this.dimensions = data['dimensions'];
       this.items = data['items'];
       this.fit = data['fit'];
+  
+      this.surveyForm = this.formBuilder.group({
+        items: this.formBuilder.array([]) // Array de items
+      });
+  
+      const itemsFormArray = this.getItemsFormArray();
+
+      this.items.forEach(item => {
+
+        console.log(item);
+
+        const itemGroup = this.formBuilder.group({
+          item: [item], 
+          dimension: ['', Validators.required],
+          fit: ['', Validators.required],
+          comentario: ''
+        });
+        (itemsFormArray?.push(itemGroup));
+      });
+
     });
   }
+  
+
 }
